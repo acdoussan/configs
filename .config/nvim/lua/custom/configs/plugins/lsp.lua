f09b4cc1beeda30be3b60cd210deef269b134ff8
Adam Doussan <adam@doussan.info>
2023-07-29 17:01:38 -0500

    remove enter binding for cmp, move unbinds into the same format as custom binds, remove conflicting C-h bind

--- .config/nvim/lua/custom/configs/plugins/lsp.lua
+++ .config/nvim/lua/custom/configs/plugins/lsp.lua
@@ -26,16 +26,16 @@ lsp.nvim_workspace()
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<Tab>'] = cmp.config.disable,
+  ['<S-Tab>'] = cmp.config.disable,
+  ['<Down>'] = cmp.config.disable,
+  ['Up'] = cmp.config.disable,
+  ['<CR>'] = cmp.config.disable,
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
-
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-cmp_mappings['<Down>'] = nil
-cmp_mappings['Up'] = nil
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
@@ -63,7 +63,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  --vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
